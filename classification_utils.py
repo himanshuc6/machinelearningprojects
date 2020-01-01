4dd1ddd10438e5461ed4c0f64de23acc4c5780ef
himanshuc6 <himanshuc6@gmail.com>
2019-12-31 19:03:57 -0800

    Add files via upload

---
+++ classification_utils.py
@@ -0,0 +1,55 @@
+def accuracy(truth_values, predicted_values):
+    """
+    Computes accuracy
+    :param truth_values: true values
+    :param predicted_values: predictions
+
+    :return: accuracy
+    """
+    ### YOUR CODE HERE ###
+
+
+ 
+
+ 
+    ######################
+
+    return accuracy
+
+def precision(truth_values, predicted_values):
+    """
+    Computes precision
+    :param truth_values: true values
+    :param predicted_values: predictions
+
+    :return: precision
+    """
+    ### YOUR CODE HERE ###
+
+
+ 
+
+ 
+    ######################
+
+    return precision
+
+
+def recall(truth_values, predicted_values):
+    """
+    Computes recall
+    :param truth_values: true values
+    :param predicted_values: predictions
+
+    :return: recall
+    """
+    ### YOUR CODE HERE ###
+
+
+ 
+
+ 
+    ######################
+
+    return recall
+
